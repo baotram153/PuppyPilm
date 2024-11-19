74e0f116a38495f484bf34f328b4fabc7251db1b
mahara0511 <122422522+mahara0511@users.noreply.github.com>
2024-11-19 21:15:33 +0700

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from "flowbite-react";
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Button } from 'flowbite-react';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <Button>Click me</Button>;
-    </>
-  )
+    return (
+        // <Router>
+        //     <div className="App">
+        //         <Routes>
+        //           <Route path="/" />
+        //           <Route path="/" />
+        //           <Route path="/" />
+        //         </Routes>
+        //     </div>
+        // </Router>
+        <button>Click Me</button>
+    );
 }
 
-export default App
+export default App;
